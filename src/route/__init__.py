e98458cf254770c807fc2aae05cfccee0eb3566d
krit pragobdee <krit.pragobdee@outlook.com>
2024-08-04 01:25:10 +0700

    update product route

--- src/route/__init__.py
+++ src/route/__init__.py
@@ -1,4 +1,5 @@
 from .admin import admin 
 from .user import user
+from .product import product
 
-__all__ = ["admin","user"]
+__all__ = ["admin","user", "product"]
--- src/route/product.py
+++ src/route/product.py
@@ -1,28 +1,60 @@
-from fastapi import APIRouter, Depends, HTTPException 
+from fastapi import APIRouter, HTTPException 
 from starlette import status 
-from datetime import timedelta
-import auth
-from typing import Annotated
-from fastapi.security import OAuth2PasswordRequestForm
-from schema.user import ClientRequest 
+
+from typing import  List
+from database.product import Products
+from schema.product import Product, CreateProduct
 from lib import db_dependency, admin_dependency
 
 
 product = APIRouter(prefix="/product", tags=["product"])
 
-async def create_product():
-    pass 
+@product.post("/", status_code=status.HTTP_201_CREATED, response_model=CreateProduct)
+async def create_product(product: CreateProduct,db: db_dependency, admin: admin_dependency):
+    _product = Products(**product.dict())
+    db.add(_product)
+    db.commit()
+    db.refresh(_product)
+    return _product 
 
+@product.patch("/{product_id}", response_model=Product)
+async def update_product(product_id: int , product: Product, db: db_dependency, admin: admin_dependency):
+    _product = db.query(Products).filter(Products.id == product_id).first()
+    if _product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
 
-async def update_product():
-    pass 
+    for k,v in product.dict().items():
+        setattr(_product,k,v)
 
-async def delete_product():
-    pass 
+    db.commit()
+    db.refresh(_product)
+    return _product
+    
+ 
 
-async def get_product():
-    pass 
+@product.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_product(product_id: int, db: db_dependency, admin: admin_dependency):
+    _product = db.query(Products).filter(Products.id == product_id).first()
+    if _product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
 
-async def get_products():
-    pass 
+    db.delete(_product)
+    db.commit()
+    return None # will chaneg to HTTP Response 
+
+@product.get("/", response_model=List[Product])
+async def get_product(db: db_dependency, admin: admin_dependency):
+    products = db.query(Products).all()
+    return products
+    
+
+@product.get("/{product_name}", response_model=Product)
+async def get_products(product_name: str ,db: db_dependency, admin: admin_dependency):
+    _product = db.query(Products).filter(Products.name == product_name).first() 
+    
+    if _product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+
+    return _product
+    
 
