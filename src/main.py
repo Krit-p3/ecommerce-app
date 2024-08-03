fb76e1d010cbbae8515720417ec92df9b949cc74
krit pragobdee <krit.pragobdee@outlook.com>
2024-08-04 01:25:37 +0700

    add product route

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from route import user, admin
+from route import user, admin, product
  
 from database.db import engine, Base 
 
 app = FastAPI()
 app.include_router(user)
 app.include_router(admin)
+app.include_router(product)
 
 
 
