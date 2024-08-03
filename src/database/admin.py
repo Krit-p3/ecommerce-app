7835fee993fb7f3cb36d3ab53a51c06d2491c245
krit pragobdee <krit.pragobdee@outlook.com>
2024-08-04 00:11:20 +0700

    refactoring database

---
+++ src/database/admin.py
@@ -0,0 +1,13 @@
+from .db import Base 
+from sqlalchemy import Column, Integer, String 
+
+
+
+class Admin(Base):
+    __tablename__ = "Admins"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
+
+
