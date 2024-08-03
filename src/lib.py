577af456e694efc78a899362122deb1239dfbc21
krit pragobdee <krit.pragobdee@outlook.com>
2024-08-03 22:30:18 +0700

    dependency

---
+++ src/lib.py
@@ -0,0 +1,19 @@
+from database import SessionLocal 
+from typing import Annotated 
+from fastapi import Depends 
+from sqlalchemy.orm import Session 
+from auth import get_current_user, get_current_admin
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+user_dependency = Annotated[Session, Depends(get_current_user)]
+
+admin_dependency = Annotated[Session, Depends(get_current_admin)]
