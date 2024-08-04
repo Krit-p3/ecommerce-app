22a8394c41166f791873cd79fc0d2a931d52ac6a
krit pragobdee <krit.pragobdee@outlook.com>
2024-08-04 11:10:33 +0700

    refactoring use service

--- src/route/admin.py
+++ src/route/admin.py
@@ -1,32 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException 
 from starlette import status 
-from datetime import timedelta
 import auth
 from typing import Annotated
 from fastapi.security import OAuth2PasswordRequestForm
 from schema.user import ClientRequest 
 from lib import db_dependency, admin_dependency
-
+from service.admin import register_admin, is_valid_admin
 
 
 admin = APIRouter(prefix='/admin', tags=['admin']) 
 @admin.post('/', status_code=status.HTTP_201_CREATED)
 async def create_admin(request: ClientRequest, db: db_dependency):
-    admin = auth.create_user(request.username, request.password, "admin")
-
-    db.add(admin)
-    db.commit()
+    res = register_admin(request.username, request.password, db)
+    return res 
 
 @admin.post('/token', response_model=auth.Token)
 async def admin_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
-    admin = auth.authenticate_admin(form_data.username, form_data.password, db)
-
-    if not admin:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = "Could't validate admin")
-
-    token = auth.create_access_token(admin.username, admin.id, timedelta(minutes=30))
-    return {'access_token': token, 'token_type': 'bearer'}
-
+    res = is_valid_admin(form_data.username, form_data.password, db)
+    return res 
 
 @admin.get("/", status_code=status.HTTP_200_OK) 
 async def _admin(admin: admin_dependency, db: db_dependency):
--- src/route/user.py
+++ src/route/user.py
@@ -1,31 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException 
 from starlette import status 
-from datetime import timedelta
 import auth
 from typing import Annotated
 from fastapi.security import OAuth2PasswordRequestForm
 from schema.user import ClientRequest 
 from lib import db_dependency, user_dependency
+from service.user import register_user, is_valid_user
 
 user = APIRouter(prefix='/user', tags=['user'])
 
 @user.post('/', status_code=status.HTTP_201_CREATED)
 async def create_user(request: ClientRequest, db: db_dependency):
-    user = auth.create_user(request.username, request.password, "users")
-
-    db.add(user)
-    db.commit()
-
-
+    res = register_user(request.username, request.password, db)
+    return res 
 
 @user.post('/token', response_model=auth.Token)
 async def user_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
-     
-    user = auth.authenticate_user(form_data.username, form_data.password,db)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = "Could't validate user")
-    token = auth.create_access_token(user.username, user.id, timedelta(minutes=30))
-    return {'access_token': token, 'token_type': 'bearer'}
+    res = is_valid_user(form_data.username, form_data.password, db)     
+    return res 
 
 @user.get("/", status_code=status.HTTP_200_OK)
 async def _user(user: user_dependency, db: db_dependency):
